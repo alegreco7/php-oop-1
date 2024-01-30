de6e40ab3f259fc99ff7f4e4377c7cff02e6b055
alegreco7 <alexgreco2002@gmail.com>
2024-01-30 16:25:38 +0100

    comments

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 class Movie
 {
+  // Proprietà della classe per memorizzare informazioni sul film
   public $title;
   public $genre;
   public $releaseYear;
 
+  // Costruttore della classe per inizializzare le proprietà del film
   public function __construct($title, $genre, $releaseYear)
   {
     $this->title = $title;
@@ -13,6 +15,7 @@ class Movie
     $this->releaseYear = $releaseYear;
   }
 
+  // Metodo per visualizzare le informazioni del film
   public function displayMovieInfo()
   {
     echo "Title: " . $this->title . "<br>";
@@ -22,11 +25,14 @@ class Movie
   }
 }
 
+// Creazione di due istanze della classe Movie con dati diversi
 $movie1 = new Movie("Titanic", "Drama", 1997);
 $movie2 = new Movie("Inception", "Thriller", 2010);
 
+// Visualizzazione delle informazioni del primo film
 echo "<h2>Movie 1:</h2>";
 $movie1->displayMovieInfo();
 
+// Visualizzazione delle informazioni del secondo film
 echo "<h2>Movie 2:</h2>";
 $movie2->displayMovieInfo();
